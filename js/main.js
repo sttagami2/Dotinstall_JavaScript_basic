b3aae12d66ebe355f7e8a8a45abdbc73f9f7293a
sttagami2 <sttagami2@gmail.com>
2020-06-06 19:54:45 +0900

    アロー関数を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 'use strict'
 
-// function sum(a, b, c) {
+// const sum = function(a, b, c) {
 //   return a + b + c;
 // }
-const sum = function(a, b, c) {
-  return a + b + c;
-}
+// const sum = (a, b, c) => {
+//   return a + b + c;
+// };
 
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-console.log(total); 
+// const sum = (a, b, c) => a + b + c;
+
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total); 
+
+
+// const double = function(a) {
+//   return a * 2;
+// };
+
+const double = a => a * 2;
+console.log(double(5));
