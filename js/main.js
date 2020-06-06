2c213b4796e4eed4142d54f2c95cbca827917577
sttagami2 <sttagami2@gmail.com>
2020-06-06 20:22:58 +0900

    splice()で配列を変更してみよう

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@
 {
   const scores = [80, 90, 40, 70];
 
-  scores.push(60, 50);  // scores配列に60と50を追加
-  scores.shift();  // 先頭の要素を削除する
+  // splice(変化が開始する位置, 削除数)
+  // splice(変化が開始する位置, 削除数, 追加する要素, ・・・)
+  scores.splice(1, 1, 40, 50);
+  // 配列の1番目を削除して、40点と50点を追加する
 
   for (let i=0; i<scores.length; i++) {
     console.log(`Score ${i+1}: ${scores[i]}`);
