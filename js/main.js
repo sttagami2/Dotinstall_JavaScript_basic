3c8653b47ea455833dd0c0b2f4e6e3f84b3da554
sttagami2 <sttagami2@gmail.com>
2020-06-07 11:18:35 +0900

    filter()を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 'use strict'
 
 {
-  const prices = [180, 190, 200];
+  const numbers = [1, 4, 7, 8, 10];
 
-  const updatePrices = prices.map(price => {
-    return price + 20;
-  });
-  
-  console.log(updatePrices);
+  // const evenNumbers = numbers.filter(number => {
+  //   if (number % 2 === 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
+  const evenNumbers = numbers.filter(number => number % 2 ===0);
+
+  console.log(evenNumbers);
 }
