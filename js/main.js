e20504542d45ebc1b4831f311f5503eea133402f
sttagami2 <sttagami2@gmail.com>
2020-06-07 11:58:17 +0900

    クラスを作ってみよう

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 'use strict'
 
 {
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount}いいね`);
+    }
+  }
+
   const posts = [
-    {
-      text: 'JavaScriptの勉強中・・・',
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
-    {
-      text: 'プログラミング楽しい',
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
+    new Post('JavaScriptの勉強中・・・'),
+    new Post('プログラミング楽しい'),
   ];
 
   posts[0].show();
