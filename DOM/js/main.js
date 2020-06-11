19aa88f3e8ffdc565fa6d24c9c6bb7b97df18a3a
sttagami2 <sttagami2@gmail.com>
2020-06-11 22:23:37 +0900

    カスタムデータ属性を扱ってみよう

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -4,16 +4,7 @@
 	document.querySelector('button').addEventListener('click', () => {
 		const targetNode = document.getElementById('target');
 
-		// targetNode.className = 'my-color my-border';
-		// targetNode.classList.add('my-color');
-
-		// #targetのクラスに.my-colorがあるかないか確認する
-		// if (targetNode.classList.contains('my-color') === true) {
-		// 	targetNode.classList.remove('my-color');
-		// } else {
-		// 	targetNode.classList.add('my-color');
-		// }
-
-		targetNode.classList.toggle('my-color');
+		// targetNode.textContent = 'Dotinstall';
+		targetNode.textContent = targetNode.dataset.translation;
 	});
 }
