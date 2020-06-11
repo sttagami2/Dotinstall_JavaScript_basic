b6a58e090cd44489c0998f782e789f778f45519e
sttagami2 <sttagami2@gmail.com>
2020-06-11 22:35:52 +0900

    要素を追加してみよう

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -2,9 +2,12 @@
 
 {
 	document.querySelector('button').addEventListener('click', () => {
-		const targetNode = document.getElementById('target');
+		const item2 = document.createElement('li');
+		item2.textContent = 'item 2';
 
-		// targetNode.textContent = 'Dotinstall';
-		targetNode.textContent = targetNode.dataset.translation;
+		// const ulNode = document.querySelector('ul');
+		const ul = document.querySelector('ul');
+
+		ul.appendChild(item2);
 	});
 }
