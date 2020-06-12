4118e83d277e74d29f9514b52d6eab8f590726ed
sttagami2 <sttagami2@gmail.com>
2020-06-12 20:55:31 +0900

    要素を削除してみよう

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -2,11 +2,11 @@
 
 {
 	document.querySelector('button').addEventListener('click', () => {
-		const item0 = document.querySelectorAll('li')[0];
-		const copy = item0.cloneNode(true);				// cloneNodeは中身を複製するもの ※falseにすると複製しない
+		const item1 = document.querySelectorAll('li')[1];
 
-		const ul = document.querySelector('ul');
-		const item2 = document.querySelectorAll('li')[2];
-		ul.insertBefore(copy, item2);							// insertBeforeにより、先ほどcopyで複製したものをitem2の前に貼り付ける
+		// item1.remove();
+		
+		document.querySelector('ul').removeChild(item1);
+		// 親Node.removeChild(削除するNode)
 	});
 }
