ccc67df9e2440428c648473c0ca8cf6beac38266
sttagami2 <sttagami2@gmail.com>
2020-06-13 17:57:17 +0900

    セレクトボックスを操作してみよう

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -3,11 +3,8 @@
 {
 	document.querySelector('button').addEventListener('click', () => {
 		const li = document.createElement('li');
-		const text = document.querySelector('input');			// 入力されたinput要素を取得
-		li.textContent = text.value;											// 入力された値をvalue属性で取得
-		document.querySelector('ul').appendChild(li);			// ulに対してliをappendChild()メソッドを使用して追加する
-
-		text.value = '';																	// appendChild()にて追加後空欄にする
-		text.focus();																		// 直ぐに入力ができるように空欄にフォーカスさせる
+		const color = document.querySelector('select');
+		li.textContent = `${color.value} - ${color.selectedIndex}`;
+		document.querySelector('ul').appendChild(li);
 	});
 }
